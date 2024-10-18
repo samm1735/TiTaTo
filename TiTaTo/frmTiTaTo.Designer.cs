b32ce4fe41d27d4271c6ad9f9ea8e2123d7af211
samm1735 <ramclief17@gmail.com>
2024-10-18 12:31:34 -0400

    Add project files.

---
+++ TiTaTo/frmTiTaTo.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace TiTaTo
+{
+    partial class frmTiTaTo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(164, 65);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(58, 59);
+            this.btn0.TabIndex = 0;
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(228, 65);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(58, 59);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(292, 65);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(58, 59);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(164, 130);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(58, 59);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(228, 130);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(58, 59);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(292, 130);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(58, 59);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(164, 195);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(58, 59);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(228, 195);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(58, 59);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(292, 195);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(58, 59);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(392, 65);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(16, 13);
+            this.lblMessage.TabIndex = 9;
+            this.lblMessage.Text = "...";
+            // 
+            // frmTiTaTo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Name = "frmTiTaTo";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ TiTaTo/frmTiTaTo.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiTaTo
+{
+    /// <summary>
+    /// Classe principale du jeu
+    /// </summary>
+    public partial class frmTiTaTo : Form
+    {
+
+            /// <summary>
+            /// Tableau représentant les positions des joueurs
+            /// </summary>
+            String[] positions = new String[9] {
+                "", "", "" ,
+                "", "", "" ,
+                "", "", ""
+            };
+            int CompteurMove = 0;
+            bool bPremierJoueur = true;
+
+            public frmTiTaTo()
+            {
+                InitializeComponent();
+            }
+            /// <summary>
+            /// Fonction permettant de calculer le gagnant
+            /// </summary>
+            /// <returns>X ou bien O</returns>
+            private String calculateWinner()
+            {
+                // Define winning combinations
+                int[,] winningCombos = new int[,] {
+                    { 0, 1, 2 },  // First row
+                    { 3, 4, 5 },  // Second row
+                    { 6, 7, 8 },  // Third row
+                    { 0, 3, 6 },  // First column
+                    { 1, 4, 7 },  // Second column
+                    { 2, 5, 8 },  // Third column
+                    { 0, 4, 8 },  // Diagonal top-left to bottom-right
+                    { 2, 4, 6 }   // Diagonal top-right to bottom-left
+                };
+
+                for (int i = 0; i < 8; i++)
+                {
+                    int a = winningCombos[i, 0];
+                    int b = winningCombos[i, 1];
+                    int c = winningCombos[i, 2];
+
+                    // Check if all three positions in a winning combo are the same and not empty
+                    if (positions[a] != "" && positions[a] == positions[b] && positions[b] == positions[c])
+                    {
+                        return positions[a]; // Return "X" or "O"
+                    }
+                }
+
+                return null; // No winner yet
+            }
+            /// <summary>
+            /// Fonction permettant de gérer le clique sur un bouton
+            /// </summary>
+            /// <param name="btn"></param>
+            /// <param name="index"></param>
+            private void GererClique(Button btn, int index)
+                {
+                    if (CompteurMove > 8)
+                        return; // Ne rien faire
+
+                    if (bPremierJoueur)
+                    {
+                        bPremierJoueur = false;
+                        positions[index] = "X";
+                        btn.Text = "X";
+                        btn.ForeColor = Color.Red;
+                        if (calculateWinner() == "X")
+                        {
+                            lblMessage.Text = "Joueur 1 a gagné !!!!";
+                            CompteurMove = 9;
+                        }
+                    }
+                    else
+                    {
+                        bPremierJoueur = true;
+                        positions[index] = "O";
+                        btn.Text = "O";
+                        btn.ForeColor = Color.Blue;
+                        if (calculateWinner() == "O")
+                        {
+                            lblMessage.Text = "Joueur 2 a gagné !!!!";
+                            CompteurMove = 9;
+                        }
+                    }
+                    CompteurMove++;
+                }
+
+            private void btn0_Click(object sender, EventArgs e)
+            {
+                GererClique(btn0, 0);
+            }
+
+            private void btn1_Click(object sender, EventArgs e)
+            {
+                GererClique(btn1, 1);
+            }
+
+            private void btn2_Click(object sender, EventArgs e)
+            {
+                GererClique(btn2, 2);
+            }
+
+            private void btn3_Click(object sender, EventArgs e)
+            {
+                GererClique(btn3, 3);
+            }
+
+            private void btn4_Click(object sender, EventArgs e)
+            {
+                GererClique(btn4, 4);
+            }
+
+            private void btn5_Click(object sender, EventArgs e)
+            {
+                GererClique(btn5, 5);
+            }
+
+            private void btn6_Click(object sender, EventArgs e)
+            {
+                GererClique(btn6, 6);
+            }
+
+            private void btn7_Click(object sender, EventArgs e)
+            {
+                GererClique(btn7, 7);
+            }
+
+            private void btn8_Click(object sender, EventArgs e)
+            {
+                GererClique(btn8, 8);
+            }
+
+            private void btnCommencez_Click(object sender, EventArgs e)
+            {
+                // Rendre les boutons prêts
+                btn0.Enabled = true; btn0.Text = "";
+                btn1.Enabled = true; btn1.Text = "";
+                btn2.Enabled = true; btn2.Text = "";
+                btn3.Enabled = true; btn3.Text = "";
+                btn4.Enabled = true; btn4.Text = "";
+                btn5.Enabled = true; btn5.Text = "";
+                btn6.Enabled = true; btn6.Text = "";
+                btn7.Enabled = true; btn7.Text = "";
+                btn8.Enabled = true; btn8.Text = "";
+                // Ré-intinialiser compteur
+                CompteurMove = 0;
+                // Ré-intinialiser positions
+                for (int i = 0; i <= 8; i++)
+                {
+                    positions[i] = "";
+                }
+                //
+                lblMessage.Text = String.Empty;
+            }
+
+            /// <summary>
+            /// Initialiser le jeu, rendre les boutons non cliquables
+            /// jusqu'à ce que le joueur clique sur le bouton Commencez
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void frmTiTaTo_Load(object sender, EventArgs e)
+            {
+                btn0.Enabled = false; btn1.Enabled = false;
+                btn2.Enabled = false; btn3.Enabled = false;
+                btn4.Enabled = false; btn5.Enabled = false;
+                btn6.Enabled = false; btn7.Enabled = false;
+                btn8.Enabled = false;
+            }
+
+       
+    }
+
+}
